9029c446d288c9d5292bd43370fa7c2e291ec5d7
Matthieu Devin <matthieu.devin@gmail.com>
2024-09-01 08:45:28 -0700

    better synonyms

--- backend/model/synonyms.go
+++ backend/model/synonyms.go
@@ -2,111 +2,110 @@ package model
 
 import (
   "log"
+  "strings"
 )
 
 type Person struct {
-  first_name string
-  last_name1 string
-  last_name2 string
-  keywords map[string]struct{}
+  name_set map[string]struct{}
 }
   
-func NewPerson(first_name string, last_name1 string,  last_name2 string) Person {
-  keywords := map[string]struct{}{}
-  keywords[first_name + " " + last_name1] = struct{}{}
-  if last_name2 !=  "" {
-    keywords[first_name + " " + last_name2] = struct{}{}
+func NewPerson(full_names ...string) *Person {
+  p := new(Person)
+  p.name_set = make(map[string]struct{}, len(full_names))
+  for _, n := range full_names {
+    p.name_set[n] = struct{}{}
   }
-  return Person{
-    first_name: first_name,
-    last_name1: last_name1,
-    last_name2: last_name2,
-    keywords: keywords,
-  }
+  return p
+}
+
+func HasName(p *Person, name string) bool {
+  _, exists := p.name_set[name]
+  return exists
 }
 
 func PersonQuery(db *Database, p *Person) Query {
-  queries := []Query{
-    KeywordQuery(db, p.first_name + " " + p.last_name1), 
-    AndQuery([]Query{
-      KeywordQuery(db, p.first_name), 
-      KeywordQuery(db, p.last_name1),
-    }),
-  }
-  if p.last_name2 != "" {
-    queries = append(queries, 
-      KeywordQuery(db, p.first_name + " " + p.last_name2),
-      AndQuery([]Query{
-        KeywordQuery(db, p.first_name), 
-        KeywordQuery(db, p.last_name2),
-      }),
-    )
+  queries := make([]Query, 2 * len(p.name_set))
+  i := 0
+  for n, _ := range p.name_set {
+    queries[i] = KeywordQuery(db, n)
+    i += 1
+    words := strings.Fields(n)
+    and_queries := make([]Query, len(words))
+    for j, w := range words {
+      and_queries[j] = KeywordQuery(db, w)
+    }
+    queries[i] = AndQuery(and_queries)
+    i += 1
   }
   return OrQuery(queries)
 }
 
-var known_people = []Person{
-  NewPerson("sofia", "moro-devin", ""),
-  NewPerson("pablo", "moro-devin", ""),
-  NewPerson("clara", "moro-devin", ""),
-  NewPerson("simon", "moro-devin", ""),
-  NewPerson("antonio", "moro-diaz", ""),
-  NewPerson("colombe", "devin", "moro-devin"),
-  NewPerson("joachim", "baudoin", ""),
-  NewPerson("philomène", "baudoin", ""),
-  NewPerson("gabriel", "baudoin", ""),
-  NewPerson("olivier", "baudoin", ""),
-  NewPerson("priscille", "devin", "baudoin"),
-  NewPerson("marguerite-marie", "sterlin", ""),
-  NewPerson("mayeul", "sterlin", ""),
-  NewPerson("amalric", "sterlin", ""),
-  NewPerson("aude", "sterlin", "tomazo"),
-  NewPerson("brunehilde", "sterlin", ""),
-  NewPerson("ombeline", "sterlin", ""),
-  NewPerson("hugues", "sterlin", ""),
-  NewPerson("claire-élise", "devin", "sterlin"),
-  NewPerson("pierre", "devin", ""),
-  NewPerson("léon", "devin", ""),
-  NewPerson("virginie", "dubos", ""),
-  NewPerson("anatole", "devin", ""),
-  NewPerson("joseph", "devin", ""),
-  NewPerson("marie", "bitschené", ""),
-  NewPerson("françois", "devin", ""),
-  NewPerson("zacharie", "devin", ""),
-  NewPerson("marin", "devin", ""),
-  NewPerson("julie", "devin", ""),
-  NewPerson("stéphanie", "peulmeul", "devin"),
-  NewPerson("samuel", "devin", ""),
-  NewPerson("élie", "devin", ""),
-  NewPerson("leïla", "devin", ""),
-  NewPerson("catherine", "doisneau", ""),
-  NewPerson("étienne", "devin", ""),
-  NewPerson("virgile", "devin", ""),
-  NewPerson("lucas", "devin", ""),
-  NewPerson("clémence", "devin", ""),
-  NewPerson("éloi", "devin", ""),
-  NewPerson("nathalie", "verbeck", "devin"),
-  NewPerson("rémi", "devin", ""),
-  NewPerson("noé", "devin", ""),
-  NewPerson("lucile", "devin", "devin-casado"),
+var known_people = []*Person{
+  NewPerson("sofia moro-devin"),
+  NewPerson("pablo moro-devin"),
+  NewPerson("clara moro-devin"),
+  NewPerson("simon moro-devin"),
+  NewPerson("antonio moro-diaz"),
+  NewPerson("colombe devin", "colombe moro-devin"),
+  NewPerson("joachim baudoin"),
+  NewPerson("philomène baudoin"),
+  NewPerson("gabriel baudoin"),
+  NewPerson("olivier baudoin"),
+  NewPerson("priscille devin", "priscille baudoin"),
+  NewPerson("marguerite-marie sterlin"),
+  NewPerson("mayeul sterlin"),
+  NewPerson("amalric sterlin"),
+  NewPerson("aude sterlin", "aude tomazo"),
+  NewPerson("brunehilde sterlin"),
+  NewPerson("ombeline sterlin"),
+  NewPerson("hugues sterlin"),
+  NewPerson("claire-élise devin", "claire-élise sterlin"),
+  NewPerson("pierre devin"),
+  NewPerson("léon devin"),
+  NewPerson("virginie dubos"),
+  NewPerson("anatole devin"),
+  NewPerson("joseph devin"),
+  NewPerson("marie bitschené"),
+  NewPerson("françois devin"),
+  NewPerson("zacharie devin"),
+  NewPerson("marin devin"),
+  NewPerson("julie devin"),
+  NewPerson("stéphanie peulmeul", "stéphanie devin"),
+  NewPerson("samuel devin"),
+  NewPerson("élie devin"),
+  NewPerson("leïla devin"),
+  NewPerson("catherine doisneau"),
+  NewPerson("étienne devin"),
+  NewPerson("virgile devin"),
+  NewPerson("lucas devin"),
+  NewPerson("clémence devin"),
+  NewPerson("éloi devin"),
+  NewPerson("nathalie verbeck", "nathalie devin"),
+  NewPerson("rémi devin"),
+  NewPerson("noé devin"),
+  NewPerson("lucile devin", "lucile devin-casado", "lucile casado"),
   // TODO: Liam!
-  NewPerson("marie", "devin", "devin-casado"),
-  NewPerson("katie", "casado", "devin-casado"),
-  NewPerson("emmanuel", "devin", ""),
-  NewPerson("coline", "devin", ""),
-  NewPerson("julien", "devin", ""),
-  NewPerson("catherine", "granger", ""),
-  NewPerson("matthieu", "devin", ""),
-  NewPerson("agnès", "devin", ""),
-  NewPerson("bernard", "devin", ""),
-  NewPerson("marisol", "devin", ""),
+  NewPerson("liam devin", "liam devin-casado", "liam casado", 
+    "marie devin", "marie devin-casado", "marie casado",),
+  NewPerson("katie casado", "katie devin-casado"),
+  NewPerson("emmanuel devin"),
+  NewPerson("coline devin"),
+  NewPerson("julien devin"),
+  NewPerson("catherine granger"),
+  NewPerson("matthieu devin"),
+  NewPerson("agnès devin"),
+  NewPerson("bernard devin"),
+  NewPerson("marisol devin"),
+  NewPerson("cécile moncla", "cécile chartier"),
+  NewPerson("bonne maman", "marcelle moncla"),
+  NewPerson("bon papa", "robert moncla", "marcel moncla"),
 }
 
 func KeywordSynonymsQuery(db *Database, kwd string) Query {
   for _, p := range known_people {
-    if _, exists := p.keywords[kwd]; exists {
+    if HasName(p, kwd) {
       log.Printf("Found a known person: %v", p)
-      return PersonQuery(db, &p)
+      return PersonQuery(db, p)
     }
   }
   return KeywordQuery(db, kwd)
